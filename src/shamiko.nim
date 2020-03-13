8818d29976125054e1f4a93d88bd7d4ad4294b8a
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-03-13 10:25:57 +0900

    Implement GET /

--- src/shamiko.nim
+++ src/shamiko.nim
@@ -1,5 +1,18 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import asynchttpserver, asyncdispatch, random
 
-when isMainModule:
-  echo("Hello, World!")
+var server = newAsyncHttpServer()
+randomize()
+
+const lines = [
+  "シャミ子が悪いんだよ",
+  "シャミ子は悪くないよ"
+]
+
+proc cb(req: Request) {.async.} =
+  if req.url.path == "/":
+    var index = rand(1)
+    await req.respond(Http200, lines[index])
+  else:
+    await req.respond(Http404, "Not Found")
+
+waitFor server.serve(Port(8080), cb)
